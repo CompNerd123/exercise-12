415340ab64ebd115804b972eab3ce222406ef4cb
Eugene <evasquez184@student.alamo.edu>
2017-03-19 21:12:59 -0500

    Update and rename Book.java to Bookshelf.java

---
+++ Bookshelf.java
@@ -0,0 +1,29 @@
+/*# exercise-12
+Eugene Vasquez
+Bookshelf
+part of exercise 12 book program
+
+*/
+
+
+ public class BookShelf {
+
+    public static void main(String [] strArgs){
+
+        
+        System.out.println("Book One= ");
+        Book objBookOne = new Book();
+
+        System.out.println("Book Two= ");
+        Book objBookTwo = new Book();
+
+        System.out.println("Book Three= ");
+        Book objBookThree = new Book();
+
+        System.out.print(objBookOne.toString());
+        System.out.print(objBookTwo.toString());
+        System.out.print(objBookThree.toString());
+        
+        
+    }
+}
